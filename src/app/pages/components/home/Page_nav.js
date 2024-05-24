d899ea2f1c819b165c5a330518853f8870fe3ebd
pawan <ps9502748@gmail.com>
2024-05-24 12:11:27 +0530

    first commit

---
+++ src/app/pages/components/home/Page_nav.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Page_nav = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Page_nav
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,31 @@
+const {nextui} = require("@nextui-org/react");
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
+    screens: {
+      'sm': '640px',
+      // => @media (min-width: 640px) { ... }
+
+      'md': '768px',
+      // => @media (min-width: 768px) { ... }
+
+      'lg': '1524px',
+      // => @media (min-width: 1024px) { ... }
+
+      'xl': '1580px',
+      // => @media (min-width: 1280px) { ... }
+
+      '2xl': '1536px',
+      // => @media (min-width: 1536px) { ... }
+    },
     extend: {
+      
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
@@ -14,5 +33,6 @@ module.exports = {
       },
     },
   },
-  plugins: [],
+  darkMode: "class",
+  plugins: [nextui()],
 };
